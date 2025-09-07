59d7051f044b8ba63966a87f9b50b409e8754a6c
wagner1067 <barra-wag@hotmail.com>
2025-09-07 20:28:20 -0300

    arrumando o import

--- routes/PacientController.js
+++ routes/PacientController.js
@@ -1,5 +1,5 @@
 import express from "express";
-import PacientService from "../services/pacientService.js";
+import PacientService from "../services/PacientService.js";
 
 const router = express.Router();
 
